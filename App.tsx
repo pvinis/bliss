e942dc685013a81f2db0064f084bfe186c893d15
Pavlos Vinieratos <pvinis@gmail.com>
2019-02-10 16:29:02 +0100

    typescript

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from './src/App'
+
+export default class ExternalApp extends React.Component {
+  render() {
+      return (
+        <App />
+      );
+  }
+
+}
